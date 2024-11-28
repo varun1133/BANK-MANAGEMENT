1976ff480f510396e7e1947153b08a8221a60c81
varun1133 <101275380+varun1133@users.noreply.github.com>
2024-11-28 22:55:28 +0530

    Add files via upload

---
+++ FRONTEND/public/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './components/Login';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Bank Management System</h1>
+            <Login />
+        </div>
+    );
+}
+
+export default App;
